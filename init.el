09974c9dd7a86af03e00579e8e23376c47d8a985
JEphron <jephron63@gmail.com>
2020-04-25 12:22:57 -0700

    Try to hide backup files

--- init.el
+++ init.el
@@ -18,6 +18,14 @@
 (setq recentf-max-menu-items 50)
 (setq recentf-max-saved-items 50)
 
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
+  backup-by-copying t    ; Don't delink hardlinks
+  version-control t      ; Use version numbers on backups
+  delete-old-versions t  ; Automatically delete excess backups
+  kept-new-versions 20   ; how many of the newest versions to keep
+  kept-old-versions 5    ; and how many of the old
+  )
+
 ;; auto-close parens and quotes
 (electric-pair-mode 1)
 
